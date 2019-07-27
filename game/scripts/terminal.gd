28f65288fadfdb39f49c0a77179f4fdab5de9e65
Raccoon-JS <raccoon-js@protonmail.com>
2019-07-27 17:19:23 -0500

    Enough work and games. Let's go out and eat.

--- game/scripts/terminal.gd
+++ game/scripts/terminal.gd
@@ -1,26 +1,28 @@
 extends Node
 
-var command = ""
+var command = "null"
 #var file_tree = {"/":["evilchild.zip"]} // map out the tree
 
 func _ready():
-	$screen/intro.add_text("root@ruinx:~" + '\n' + "$ Enter the command.\n")
+	$screen/intro.add_text("AVAILABLE COMMANDS: quit, clear, list, cd, help, and unzip.\n" + "root@ruinx:~\n" + "$\n")
 	pass # Replace with function body.
 
 func _input(event):
 	if event.is_action_pressed("enter"):
 		command = $command_line.text
-		if command != "clear":
-			$screen/intro.add_text("root@ruinx:~" + '\n' + "$ " + command + '\n')
+		
+		$screen/intro.add_text("root@ruinx:~" + '\n' + "$ " + command + '\n')
+		if command == "quit":
+			get_tree().quit()
+		elif command == "clear":
+			$screen/intro.clear()
+			$screen/intro.add_text("root@ruinx:~" + '\n' + "$ clear\n")
+		else:
+			$screen/intro.add_text("UNRECOGNIZABLE COMMAND LINE.\nTry entering AVAILABLE COMMANDS: quit, clear, list, cd, help, and unzip.\n")
 	if event.is_action_released("enter"):
 		$command_line.text = ""
 	pass
 
-func _process(delta):
-	if delta:
-		if command == "quit":
-			get_tree().quit()
-		if command == "clear":
-			$screen/intro.clear()
-			$screen/intro.add_text("root@ruinx:~" + '\n' + "$ clear\n")
-	pass
+#func _process(delta):
+#	if delta:
+#		
